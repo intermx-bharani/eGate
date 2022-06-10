edf0132bad5bee7ff6807f16402522eea5c7aeb9
agiratech-kaviya <kaviya.s@agiratech.com>
2022-06-10 16:37:10 +0530

    changed

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res, next) {
   res.send('Home page');
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log("http://localhost:3000");
+    console.log("http://localhost:5000");
   });
