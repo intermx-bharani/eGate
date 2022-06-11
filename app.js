7bceb5d78209c07200aa70ab46c6a09c7941587e
agiratech-kaviya <kaviya.s@agiratech.com>
2022-06-11 14:18:55 +0530

    add morgan

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');   
 const database = require("./config/mongo");
 require('dotenv').config()
 const bodyparser = require("body-parser");
 
 
 const app = express();
+app.use(morgan())
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
